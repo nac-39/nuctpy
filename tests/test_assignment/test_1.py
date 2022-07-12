e75e1fb84023ce9680c4cd76b7eb19be75ff717b
nac-39 <83295760+nac-39@users.noreply.github.com>
2022-07-12 17:51:39 +0900

    #5 [add] test

--- tests/test_assignment/test_1.py
+++ tests/test_assignment/test_1.py
@@ -2,7 +2,12 @@ from NUCT import Assignment
 
 def test_assignment_site():
     a = Assignment()
-    a.my()
-    assert type(a.site("2022_1002030")) is list
-    a.site("hgoehoge")
-    a.item("")
+    # 物質情報学1
+    assert a.site("2022_1002030")[0]["title"] == "課題5（2022年7月11日出題）"
+    # 微積分学の発展
+    assert a.site("2022_1001030")[0]["title"] == "第5回課題"
+    
+def test_assignment_my():
+    a = Assignment()
+    assert a.my()[0]["title"] == "第5回課題"
+
