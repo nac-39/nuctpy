044e4c23e61478eb1caa3d68fdc2bfd94be06027
nac-39 <83295760+nac-39@users.noreply.github.com>
2022-07-12 17:33:54 +0900

    #5 [wip] テストが実行できるようになった

--- tests/test_assignment/test_1.py
+++ tests/test_assignment/test_1.py
@@ -2,4 +2,7 @@ from NUCT import Assignment
 
 def test_assignment_site():
     a = Assignment()
-    print(a)
+    a.my()
+    assert type(a.site("2022_1002030")) is list
+    a.site("hgoehoge")
+    a.item("")
