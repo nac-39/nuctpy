59596817f8bb0da48630f5711954da4f3ebf71e9
nac-39 <83295760+nac-39@users.noreply.github.com>
2022-07-23 02:47:04 +0900

    #12 [add] content

--- NUCT/cli.py
+++ NUCT/cli.py
@@ -1,13 +1,18 @@
+from operator import is_
 import click
 import datetime
-from .nuct import NUCT
+import NUCT
+
+@click.group()
+def nuct():
+    print("hello")
 
 @click.option("--all", "-a", default=False, is_flag=True)
 @click.option("--detail", "-d", default=False, is_flag=True)
 @click.option("--id", "-i", default=False, is_flag=True)
 @click.option("--year", "-y", default=None)
-@click.command()
-def nuct(all, detail, id, year):
+@nuct.command()
+def site(all, detail, id, year):
     nuct = NUCT()
     _data = nuct.site_data
     if all:
@@ -26,3 +31,22 @@ def nuct(all, detail, id, year):
         if detail:
             string += "\n\t" + d["description"]
         click.echo(string)
+
+
+@click.option("--grep", "-g", default="", help="リソースの名前の一部を指定します")
+@click.option("--download", "-d", default=False, is_flag=True, help="コンテンツを全てダウンロードします")
+@click.option("--link", "-l", default=False, is_flag=True, help="URLを表示します")
+@click.argument("siteid")
+@nuct.command()
+def content(siteid, link, download, grep):
+    c = NUCT.Content()    
+    data = c.site(siteid)
+    for d in data:
+        dl = False
+        if download:
+            if (grep in d["title"]) and (not d["url"].split("/")[-1] == ""):
+                c.load_contents([d["url"]])
+                dl = True
+        click.echo("💾: "*dl + d["entityTitle"] + ("\t" + d["url"])*link)
+
+    
