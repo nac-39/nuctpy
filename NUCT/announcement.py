af0f71a52f45b87764cb2ba5492b59588c7ea4c9
nac-39 <83295760+nac-39@users.noreply.github.com>
2022-08-21 20:53:58 +0900

    #22 [add] announcement class

---
+++ NUCT/announcement.py
@@ -0,0 +1,49 @@
+"""Announcement
+お知らせを取得するためのクラスです。
+"""
+from .nuct import NUCT
+
+
+class Announcement(NUCT):
+    def __init__(self, session=None):
+        super().__init__(session)
+        self.announcement_url = self._urls.direct + "/announcement"
+
+    @NUCT.formatter
+    def site(self, siteid, format="json"):
+        """ある授業のお知らせを取得する関数です。
+        Args:
+            siteid: string 授業のサイトID
+            format: "json"|"xml" 出力の形式
+
+        Returns:
+            dict: 一覧を辞書の配列で返す
+        """
+        url = self.announcement_url + f"/{siteid}.{format}"
+        res = self.session.get(url)
+        return res
+
+    @NUCT.formatter
+    def motd(self, format="json"):
+        """今日のお知らせの一覧を取得する関数です。
+        Args:
+            format: "json"|"xml" 出力の形式
+        Returns:
+            dict: 一覧を辞書の配列で返す
+        """
+        url = self.announcement_url + f"/motd.{format}"
+        res = self.session.get(url)
+        return res
+
+    @NUCT.formatter
+    def user(self, format="json"):
+        """あるユーザーのお知らせの一覧を取得する関数です
+         Args:
+            format: "json"|"xml" 出力の形式
+        Returns:
+            dict: 一覧を辞書の配列で返す
+
+        """
+        url = self.announcement_url + f"/user.{format}"
+        res = self.session.get(url)
+        return res
