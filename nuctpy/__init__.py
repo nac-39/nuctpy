f205e8309af00a6996bad0522cb8ab43e1d8dc94
nac-39 <83295760+nac-39@users.noreply.github.com>
2022-10-06 13:06:22 +0900

    #55 テストを追加

--- nuctpy/__init__.py
+++ nuctpy/__init__.py
@@ -1,3 +1,13 @@
 from .api import NUCT, Announcement, Assignment, Content, Quiz, Roster
+from .lecture import Lecture, get_lectures
 
-__all__ = ["Announcement", "Assignment", "Content", "NUCT", "Quiz", "Roster"]
+__all__ = [
+    "Announcement",
+    "Assignment",
+    "Content",
+    "NUCT",
+    "Quiz",
+    "Roster",
+    "get_lectures",
+    "Lecture",
+]
